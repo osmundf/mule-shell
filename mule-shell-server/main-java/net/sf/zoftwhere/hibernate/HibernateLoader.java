041ed21257ff127fd45802c7b5f35e01fd63c794
Osmund Francis <osmund.francis@gmail.com>
2019-07-25 14:07:23 +0200

    Updated with naming strategy.

--- mule-shell-server/main-java/net/sf/zoftwhere/hibernate/HibernateLoader.java
+++ mule-shell-server/main-java/net/sf/zoftwhere/hibernate/HibernateLoader.java
@@ -8,9 +8,22 @@ import java.util.Map;
 public class HibernateLoader {
 
 	public static Configuration getConfiguration(final Map<String, String> propertyMap, List<Class<?>> entityList) {
-		Configuration configuration = new Configuration();
+		final Configuration configuration = new Configuration();
 		propertyMap.forEach(configuration::setProperty);
 		entityList.forEach(configuration::addAnnotatedClass);
+		return configuration;
+	}
+
+	public static Configuration defaultH2TestDatabase(List<Class<?>> entityList) {
+		final Configuration configuration = new Configuration();
+		entityList.forEach(configuration::addAnnotatedClass);
+		configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem:test;mode=PostgreSQL;database_to_lower=true");
+		configuration.getProperties().setProperty("hibernate.connection.username", "admin");
+		configuration.getProperties().setProperty("hibernate.connection.password", "");
+		configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+		configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL10Dialect");
+		configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+		configuration.setPhysicalNamingStrategy(new SnakeCaseNamingStrategy());
 		return configuration;
 	}
 }
