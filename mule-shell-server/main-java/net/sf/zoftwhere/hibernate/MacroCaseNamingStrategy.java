f6143420fe312596f4d2101e806e06198403e06b
Osmund Francis <osmund.francis@gmail.com>
2019-07-25 14:05:56 +0200

    Hibernate macro-case naming strategy.

---
+++ mule-shell-server/main-java/net/sf/zoftwhere/hibernate/MacroCaseNamingStrategy.java
@@ -0,0 +1,44 @@
+package net.sf.zoftwhere.hibernate;
+
+import org.hibernate.boot.model.naming.Identifier;
+import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
+
+public class MacroCaseNamingStrategy extends SnakeCaseNamingStrategy {
+
+	@Override
+	public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+		return convertToMacroCase(identifier);
+	}
+
+	@Override
+	public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+		return convertToMacroCase(identifier);
+	}
+
+	@Override
+	public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+		return convertToMacroCase(identifier);
+	}
+
+	@Override
+	public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+		return convertToMacroCase(identifier);
+	}
+
+	@Override
+	public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnv) {
+		return convertToMacroCase(identifier);
+	}
+
+	protected Identifier convertToMacroCase(final Identifier identifier) {
+		if (identifier == null) {
+			return null;
+		}
+
+		return Identifier.toIdentifier(macroCase(identifier.getText()), identifier.isQuoted());
+	}
+
+	protected String macroCase(String input) {
+		return snakeCase(input).toUpperCase();
+	}
+}
