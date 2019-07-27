c54f614eddaae5227e0d86b02b35e605acb85850
Osmund <osmund.francis@gmail.com>
2019-07-27 13:47:37 +0200

    Updated the dependencies, and output of logging. (#19)

--- mule-shell-model/test-java/net/sf/zoftwhere/mule/model/AccessRoleModelTest.java
+++ mule-shell-model/test-java/net/sf/zoftwhere/mule/model/AccessRoleModelTest.java
@@ -1,10 +1,8 @@
 package net.sf.zoftwhere.mule.model;
 
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -12,18 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AccessRoleModelTest {
 
-	public static void main(String[] args) {
-		final var valueArray = AccessRoleModel.values();
-		final var size = valueArray.length;
-		final var idArray = newIdArray(valueArray.length);
-
-		for (int i = 0; i < size; i++) {
-			final var e = valueArray[i];
-			final var id = idArray[i];
-			final var key = getKey(e);
-			System.out.printf("%s %s %s%n", id.toString(), e.name(), key);
-		}
-	}
+	private static Logger logger = LoggerFactory.getLogger(AccessRoleModelTest.class);
 
 	@Test
 	void ensureBasicRolesAreAvailable() {
@@ -37,20 +24,7 @@ public class AccessRoleModelTest {
 		assertTrue(model != null, "Must not be null.");
 		assertEquals("net.sf.zoftwhere.mule.model", model.getClass().getPackage().getName(), "Constant package");
 		assertNotNull(model.name());
-	}
-
-	private static UUID[] newIdArray(int length) {
-		final var array = new UUID[length];
-		for (int i = 0; i < length; i++) {
-			array[i] = UUID.randomUUID();
-		}
-		Arrays.sort(array, Comparator.comparing(UUID::toString));
-		return array;
-	}
-
-	private static String getKey(AccessRoleModel model) {
-		final var packageName = model.getClass().getPackage().getName();
-		final var enumName = model.name();
-		return packageName + ":" + enumName;
+		
+		logger.debug("Ensured access key model enum " + model.name());
 	}
 }
