33f2b62202de44fdcaeb73c69fb920d852969f41
Osmund Francis <osmund.francis@gmail.com>
2019-08-02 13:48:44 +0200

    Removed unused method in secure module.

--- mule-shell-server/main-java/net/sf/zoftwhere/mule/security/SecureModule.java
+++ mule-shell-server/main-java/net/sf/zoftwhere/mule/security/SecureModule.java
@@ -2,7 +2,6 @@ package net.sf.zoftwhere.mule.security;
 
 import com.auth0.jwt.JWTVerifier;
 import com.google.inject.Provides;
-import io.dropwizard.setup.Bootstrap;
 import net.sf.zoftwhere.mule.MuleConfiguration;
 import ru.vyarus.dropwizard.guice.module.support.DropwizardAwareModule;
 
@@ -17,11 +16,6 @@ public class SecureModule extends DropwizardAwareModule<MuleConfiguration> {
 	public SecureModule(Supplier<JWTSigner> signerProducer, Supplier<JWTVerifier> verifierProducer) {
 		this.verifier = verifierProducer;
 		this.signer = signerProducer;
-	}
-
-	@Override
-	protected Bootstrap<MuleConfiguration> bootstrap() {
-		return super.bootstrap();
 	}
 
 	@Provides
